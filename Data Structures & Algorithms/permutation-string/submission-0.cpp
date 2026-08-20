ec6fe6fb9c47c8fdfcb3773a13c6bbd189baf831
harshit ambati <150409324+Harshit-ambati@users.noreply.github.com>
2026-08-20 22:18:46 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> map;
+        bool found = false;
+        for(int i = 0;i<s1.size();i++){
+            map[s1[i]]++;
+        }
+        int n = s1.size();
+        int i = 0;
+        int j = i + n;
+        while(j<=s2.size()){
+                    unordered_map<char,int>map1;
+                            bool ans = true;
+
+            for(int k = i;k<j;k++){
+                map1[s2[k]]++;
+            }
+            for(int k = 0;k<n;k++){
+                map1[s1[k]]--;
+            }
+            for(auto &[element,count] : map1){
+                if(count != 0){
+                    ans = false;
+                }
+            }
+            if(ans == true){
+                found = true;
+                break;
+            }
+            i++;
+            j++;
+        }
+        return found;
+    }
+};
